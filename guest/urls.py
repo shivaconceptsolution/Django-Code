5de71c68c9133de076eda9f339d62e325fb9209f
shivaconceptsolution <50239824+shivaconceptsolution@users.noreply.github.com>
2019-06-21 11:13:09 +0530

    Add files via upload

--- guest/urls.py
+++ guest/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
      path('', views.index, name='index'),
      path('index', views.index, name='index'),
      path('addstudent', views.addstudent, name='addstudent'),
+     path('viewstudent', views.viewstudent, name='viewstudent'),
+     path('Editstudent', views.Editstudent, name='Editstudent'),
+     path('Deletestudent', views.Deletestudent, name='Deletestudent'),
      path('si', views.si, name='si'),
 ]
--- guest/views.py
+++ guest/views.py
@@ -8,7 +8,16 @@ def addstudent(request):
    s=Student(rno=request.POST["txtrno"],sname=request.POST["txtname"],branch=request.POST["txtbranch"],fees=int(request.POST["txtfees"]))
    s.save()	
    return HttpResponse("data insert successfully")
-
+def viewstudent(request):
+  s=Student.objects.all()
+  return render(request,"guest/viewstudent.html",{'key':s})
+def Editstudent(request):
+ s = Student.objects.get(pk=int(request.GET["q"])) 
+ return render(request,"guest/editstudent.html",{'key':s})
+def Deletestudent(request):
+  s = Student.objects.get(pk=int(request.GET["q"]))
+  s.delete()
+  return HttpResponse("Record Deleted ")    
 def si(request):
     p=120000
     r=2.2
